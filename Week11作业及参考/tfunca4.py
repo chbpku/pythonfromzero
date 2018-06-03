30702e2648438c4839c072e699c37f489e78a33f
ChenBin <chbpku@gmail.com>
2018-06-03 19:48:38 +0800

    week11

---
+++ Week11作业及参考/tfunca4.py
@@ -0,0 +1,30 @@
+import turtle  # 导入海龟制图模块
+import random  # 导入随机模块
+
+colors = ['blue', 'green', 'red', 'pink', 'brown', 'yellow', 'orange']
+t = turtle.Turtle()  # 生成一个海龟，名字叫“t”
+t.pensize(2)
+
+
+# 随机颜色的用法：在需要用到颜色的地方写：
+# random.choice(colors)
+
+# 在下面开始写你的代码，以上的代码勿动
+# =============
+
+# 一个边长是m,n的长方形
+def rectangle(m, n):
+    for i in range(2):
+        t.forward(m)
+        t.right(90)
+        t.forward(n)
+        t.right(90)
+
+
+rectangle(100, 30)
+
+# =============
+# 你的代码结束，以下的代码勿动
+
+t.hideturtle()  # 隐藏海龟
+turtle.done()  # 海龟作图完成，等待欣赏
