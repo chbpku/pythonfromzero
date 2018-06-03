b454606f0c292771b51153938c314d68c363f218
ChenBin <chbpku@gmail.com>
2018-06-03 12:39:59 +0800

    提交week10文件。

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # pythonfromzero
 零基础编程学习
 
+# 编程模版
+- t3.py：具有随机颜色的海龟作图编程模版
+
 # Python开发环境
 ## thonny下载
 [请点击链接前往下载](http://thonny.org/)
--- 编程模版/t3.py
+++ 编程模版/t3.py
@@ -5,12 +5,15 @@ colors = ['blue', 'green', 'red', 'pink', 'brown', 'yellow', 'orange']
 t = turtle.Turtle()  # 生成一个海龟，名字叫“t”
 t.pensize(2)
 
-# 开始写你的代码
+# 随机颜色的用法：在需要用到颜色的地方写：
+# random.choice(colors)
+
+# 在下面开始写你的代码，以上的代码勿动
 # =============
 
 
 # =============
-# 代码结束
+# 你的代码结束，以下的代码勿动
 
-t.hideturtle()  # 隐藏海):龟
+t.hideturtle()  # 隐藏海龟
 turtle.done()  # 海龟作图完成，等待欣赏
